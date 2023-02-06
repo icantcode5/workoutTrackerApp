4fd9e77c18632cd53c1955651fdddafb1e1fd266
Carlos Sosa <carlossosa54321@gmail.com>
2023-02-06 01:19:10 -0800

    cleaned up server file code and fixed redirects file name

--- server.js
+++ server.js
@@ -1,83 +1,56 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-const cors = require('cors')
-const mongoose = require("mongoose");
-//const passport = require("passport");
-const session = require("express-session");
-const MongoStore = require("connect-mongo")//(session); no longer need this in the new version of MongoStore
-//const methodOverride = require("method-override");
-//const flash = require("express-flash"); 
-//const logger = require("morgan");
-const connectDB = require("./config/database");
+const cors = require("cors")
+const mongoose = require("mongoose")
+const session = require("express-session")
+const MongoStore = require("connect-mongo") //(session); no longer need this in the new version of MongoStore
+const connectDB = require("./config/database")
 //const mainRoutes = require("./routes/main");
 const workoutRoutes = require("./routes/workout")
-const userRoutes = require("./routes/user");
+const userRoutes = require("./routes/user")
 const testRoute = require("./routes/test")
 const path = require("path")
 
-
 //Use .env file in config folder
-require("dotenv").config({ path: "./config/.env" });
- 
-// Passport config
-//require("./config/passport")(passport);
+require("dotenv").config({ path: "./config/.env" })
 
 //Connect To Database
-connectDB(); 
-
-//Using EJS for views
-//app.set("view engine", "html"); //changed from ejs to html
+connectDB()
 
 //Static Folder
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static("./client/build"));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("./client/build"))
 }
 
-
-
 //Body Parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use(cors())
-
-//Logging
-//app.use(logger("dev"));
-
-//Use forms for put / delete
-//app.use(methodOverride("_method"));
 
 // Setup Sessions - stored in MongoDB
 app.use(
-  session({
-    secret: "keyboard cat",
-    resave: false, 
-    saveUninitialized: false,
-    //store property has been updated and can be found in docs
-    store: MongoStore.create( { mongoUrl: process.env.DB_STRING } ),
-  })
-);
-
-// Passport middleware
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-//Use flash messages for errors, info, ect...
-//app.use(flash());
+	session({
+		secret: "keyboard cat",
+		resave: false,
+		saveUninitialized: false,
+		//store property has been updated and can be found in docs
+		store: MongoStore.create({ mongoUrl: process.env.DB_STRING }),
+	})
+)
 
 //Setup Routes For Which The Server Is Listening
 app.use("/", testRoute)
-app.use("/users", userRoutes);
-app.use("/workout", workoutRoutes);
+app.use("/users", userRoutes)
+app.use("/workout", workoutRoutes)
 //app.use("/comment", commentsRoutes)
 
-if (process.env.NODE_ENV === "production"){
-  app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"))
-  })
+if (process.env.NODE_ENV === "production") {
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "./client/build/index.html"))
+	})
 }
 
 //Server Running
 app.listen(process.env.PORT, () => {
-  console.log("Server is running, you better catch it!");
-});
-
+	console.log("Server is running, you better catch it!")
+})
